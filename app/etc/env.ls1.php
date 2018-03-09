28f93216ff2a5d7ea56aacc57c0dda1eaff6f76c
xuan <xuan@serfe.com>
2018-03-09 16:06:59 -0300

    0052669 - update configuration files for development environment

---
+++ app/etc/env.ls1.php
@@ -0,0 +1,55 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => '15832f59e82df8818c41bae6f272a88d',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => '<db-service-name>',
+        'dbname' => '<db-name>',
+        'username' => '<db-username>',
+        'password' => '<db-password>',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'customer_notification' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'full_page' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+  ),
+  'install' => 
+  array (
+    'date' => 'Thu, 01 Mar 2018 22:00:58 +0000',
+  ),
+);
