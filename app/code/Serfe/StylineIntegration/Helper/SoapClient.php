9a47c7729a4c8a91bab61105439feb1ad7edae83
xuan <xuan@serfe.com>
2018-03-23 14:44:05 -0300

    0052819 - add support for new configurations

--- app/code/Serfe/StylineIntegration/Helper/SoapClient.php
+++ app/code/Serfe/StylineIntegration/Helper/SoapClient.php
@@ -12,7 +12,32 @@ class SoapClient extends \Magento\Framework\App\Helper\AbstractHelper
     const PRODUCTION_WSDL_CONFIG_PATH = "styline_integration/setting/wsdl_url_production";
     const TEST_WSDL_CONFIG_PATH = "styline_integration/setting/wsdl_url_test";
     const TEST_MODE_CONFIG_PATH = "styline_integration/setting/test_mode";
+    const SOAP_VERSION = "styline_integration/setting/soap_version";
     
+    protected $storeManager;
+    
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Store\Model\StoreManagerInterface $storeMaganger
+     */
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Store\Model\StoreManagerInterface $storeMaganger
+    ) {
+        $this->storeManager = $storeMaganger;
+        
+        parent::__construct($context);
+    }
+    
+    /**
+     * Executes a request to the SOAP API
+     *
+     * @param string $callable
+     * @param mixed $data
+     * @return mixed
+     */
     protected function execRequest($callable, $data)
     {
         $wsdl = $this->getWsdl();
@@ -28,31 +53,44 @@ class SoapClient extends \Magento\Framework\App\Helper\AbstractHelper
         return $response;
     }
     
+    /**
+     * Get configured value for current website
+     *
+     * @param string $configPath
+     * @return mixed
+     */
+    protected function getConfigValue($configPath)
+    {
+        $websiteCode = $this->storeManager->getWebsite()->getCode();
+        return $this->scopeConfig->getValue($configPath, \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE, $websiteCode);
+    }
+    
+    /**
+     * Get WSDL URL
+     *
+     * @return string
+     */
     protected function getWsdl()
     {
-        $testMode = $this->scopeConfig->getValue($this::TEST_MODE_CONFIG_PATH);
+        $testMode = $this->getConfigValue($this::TEST_MODE_CONFIG_PATH);
         $wsdlConfig = $this::PRODUCTION_WSDL_CONFIG_PATH;
         if ($testMode) {
             $wsdlConfig = $this::TEST_WSDL_CONFIG_PATH;
         }
-        return $this->scopeConfig->getValue($wsdlConfig);
+        return $this->getConfigValue($wsdlConfig);
     }
     
+    /**
+     * Get options for the SOAP Client
+     *
+     * @return array
+     */
     protected function getOptions()
     {
-        return [
-            'soap_version' => SOAP_1_1
-        ];
-    }
-    
-    public function getPartInfo($partNumber, $qty, $customerId)
-    {
-        $partInfo = new \stdClass();
-        $partInfo->ErpGetPartInfoRequest = new \stdClass();
-        $partInfo->ErpGetPartInfoRequest->PartNumber = $partNumber;
-        $partInfo->ErpGetPartInfoRequest->Quantity = $qty;
-        $partInfo->ErpGetPartInfoRequest->CustomerId = $customerId;
+        $soapVersion = $this->getConfigValue($this::SOAP_VERSION) == 'SOAP_1_1'? SOAP_1_1 : SOAP_1_2;
 
-        return $this->execRequest("GetPartInfo",$partInfo);
+        return [
+            'soap_version' => $soapVersion
+        ];
     }
 }
