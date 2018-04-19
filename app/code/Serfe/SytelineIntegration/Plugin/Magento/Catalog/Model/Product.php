4b1c39bcae8c1914876763812b02c8279770f1fc
xuan <xuan@serfe.com>
2018-04-19 10:27:26 -0300

    0052819 - modify plugin to verify if product exists on syteline

--- app/code/Serfe/SytelineIntegration/Plugin/Magento/Catalog/Model/Product.php
+++ app/code/Serfe/SytelineIntegration/Plugin/Magento/Catalog/Model/Product.php
@@ -9,18 +9,62 @@ namespace Serfe\SytelineIntegration\Plugin\Magento\Catalog\Model;
  */
 class Product
 {
+    /**
+     * Syteline  Helper
+     *
+     * @var \Serfe\SytelineIntegration\Helper\SytelineHelper 
+     */
     protected $sytelineHelper;
 
+    /**
+     * Product Repository
+     *
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface 
+     */
+    protected $productRepository;
+
     public function __construct(
-        \Serfe\SytelineIntegration\Helper\SytelineHelper $sytelineHelper
+        \Serfe\SytelineIntegration\Helper\SytelineHelper $sytelineHelper,
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
     ) {
         $this->sytelineHelper = $sytelineHelper;
+        $this->productRepository = $productRepository;
     }
 
+    /**
+     * isAvailable Plugin
+     *
+     * @param \Magento\Catalog\Model\Product $subject
+     * @param boolean $result
+     * @return boolean
+     */
     public function afterIsAvailable(
         \Magento\Catalog\Model\Product $subject,
         $result
     ) {
-        return $this->sytelineHelper->isProductAvailable($subject);
+        $returnValue = $result;
+        if ($this->existsInSyteline($subject)) {
+            $returnValue = $this->sytelineHelper->isProductAvailable($subject);
+        }
+
+        return $returnValue;
+    }
+
+    /**
+     * Check if $product exists in Syteline
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @return boolean
+     */
+    protected function existsInSyteline($product)
+    {
+        try {
+            $loadedProduct = $this->productRepository->getById($product->getId());
+            $exists = (bool) $loadedProduct->getExistsInSyteline();
+        } catch (Exception $ex) {
+            $exists = false;
+        }
+
+        return $exists;
     }
 }
