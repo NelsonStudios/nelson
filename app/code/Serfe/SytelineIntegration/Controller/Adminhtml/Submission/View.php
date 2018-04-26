1f0721f83fe434b8128a523d8b73a8976a632250
xuan <xuan@serfe.com>
2018-04-26 08:53:17 -0300

    0052819 - add controller to view syteline cart submission

---
+++ app/code/Serfe/SytelineIntegration/Controller/Adminhtml/Submission/View.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Serfe\SytelineIntegration\Controller\Adminhtml\Submission;
+
+/**
+ * View Submission on Backend
+ *
+ * @author Xuan Villagran <xuan@serfe.com>
+ */
+class View extends \Magento\Backend\App\Action
+{
+    /**
+     * Page Factory
+     *
+     * @var \Magento\Framework\View\Result\PageFactory 
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Submission Repository
+     *
+     * @var \Serfe\SytelineIntegration\Api\SubmissionRepositoryInterface 
+     */
+    protected $submissionRepository;
+
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $coreRegistry;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context                           $context
+     * @param \Magento\Framework\View\Result\PageFactory                    $resultPageFactory
+     * @param \Serfe\SytelineIntegration\Api\SubmissionRepositoryInterface  $submissionRepository
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context                             $context,
+        \Magento\Framework\View\Result\PageFactory                      $resultPageFactory,
+        \Magento\Framework\Registry                                     $coreRegistry,
+        \Serfe\SytelineIntegration\Api\SubmissionRepositoryInterface    $submissionRepository
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        $this->submissionRepository = $submissionRepository;
+        $this->coreRegistry = $coreRegistry;
+        parent::__construct($context);
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $submissionId = $this->getRequest()->getParam('submission_id');
+        $submissionLoaded = $this->loadSubmission($submissionId);
+
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__("View Submission"));
+        if (!$submissionLoaded) {
+            $this->messageManager->addError(__('This submission no longer exists.'));
+            $this->_redirect('*/submission/');
+            $resultPage = null;
+        }
+
+        return $resultPage;
+    }
+
+    /**
+     * Load Submission into Core Registry
+     *
+     * @param string $submissionId
+     * @return boolean
+     */
+    protected function loadSubmission($submissionId)
+    {
+        try {
+            $submission = $this->submissionRepository->getById($submissionId);
+            $this->coreRegistry->register('submission', $submission);
+            $success = true;
+        } catch (\Magento\Framework\Exception\LocalizedException $ex) {
+            $success = false;
+        }
+
+        return $success;
+    }
+}
