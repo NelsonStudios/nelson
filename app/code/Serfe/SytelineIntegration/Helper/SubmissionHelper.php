863683847fcd30b89d8d7668757fb5407d05e1c8
xuan <xuan@serfe.com>
2018-04-26 08:42:57 -0300

    0052819 - add php comments - fix error on save (array to string conversion)

--- app/code/Serfe/SytelineIntegration/Helper/SubmissionHelper.php
+++ app/code/Serfe/SytelineIntegration/Helper/SubmissionHelper.php
@@ -30,36 +30,58 @@ class SubmissionHelper extends \Magento\Framework\App\Helper\AbstractHelper
      */
     protected $configHelper;
 
+    /**
+     * Logger
+     *
+     * @var \Serfe\SytelineIntegration\Logger\Handler 
+     */
+    protected $logger;
+
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
         \Serfe\SytelineIntegration\Model\SubmissionFactory $submissionFactory,
         \Serfe\SytelineIntegration\Api\SubmissionRepositoryInterface $submissionRepository,
-        \Serfe\SytelineIntegration\Helper\ConfigHelper $configHelper
+        \Serfe\SytelineIntegration\Helper\ConfigHelper $configHelper,
+        \Serfe\SytelineIntegration\Logger\Logger $logger
     ) {
         parent::__construct($context);
 
         $this->submissionFactory = $submissionFactory;
         $this->submissionRepository = $submissionRepository;
         $this->configHelper = $configHelper;
+        $this->logger = $logger;
     }
 
-    public function createSubmission($request, $response, $successfullRequest, $errors = null)
+    /**
+     * Create and save a Submission entity
+     *
+     * @param string $orderId
+     * @param array $request
+     * @param \stdClass $response
+     * @param boolean $successfullRequest
+     * @param array $errors
+     * @return boolean
+     */
+    public function createSubmission($orderId, $request, $response, $successfullRequest, $errors = null)
     {
         $submission = $this->submissionFactory->create();
         $testingMode = $this->configHelper->isTestModeEnabled();
         $requestStr = print_r($request, true);
         $responseStr = print_r($response, true);
+        $errorsStr = $errors ? print_r($errors, true) : $errors;
+        $submission->setOrderId($orderId);
         $submission->setSuccess($successfullRequest);
         $submission->setTesting($testingMode);
         $submission->setRequest($requestStr);
         $submission->setResponse($responseStr);
-        $submission->setErrors($errors);
+        $submission->setErrors($errorsStr);
 
         try {
             $this->submissionRepository->save($submission);
             $success = true;
         } catch (\Magento\Framework\Exception\LocalizedException $ex) {
             $success = false;
+            $this->logger->err('Could not create submission entity for order id: ' . $orderId . ', exception message: ' . $ex->getMessage());
         }
 
         return $success;
