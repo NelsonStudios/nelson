31bf744f6d9cd3191242691ab4e6335a21c41ce1
xuan <xuan@serfe.com>
2018-04-30 18:16:29 -0300

    0053814 - fix requireJS configuration in order to disable merge of js files

--- app/design/frontend/Serfe/fecon/requirejs-config.js
+++ app/design/frontend/Serfe/fecon/requirejs-config.js
@@ -4,5 +4,26 @@ var config = {
         '*': {
             "Instafeed": "js/library/instafeed.min"
         }
+    },
+    shim: {
+        jquery: {
+            exports: '$'
+        },
+        'Smartwave_Megamenu/js/sw_megamenu':
+            {
+                deps: ['jquery']
+            }, 
+        'owl.carousel/owl.carousel.min':
+            {
+                deps: ['jquery']
+            },
+        'js/jquery.stellar.min': 
+            {
+            deps: ['jquery']
+            },
+        'js/jquery.parallax.min':
+            {
+            deps: ['jquery']
+            }
     }
 };
