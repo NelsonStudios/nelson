aab928b673cc7c7e389077bb64f67b618d2fbb3b
xuan <xuan@serfe.com>
2018-05-02 09:59:17 -0300

    0053814 - add Magento_Catalog/js/jquery.zoom.min into requireJS config

--- app/design/frontend/Serfe/fecon/requirejs-config.js
+++ app/design/frontend/Serfe/fecon/requirejs-config.js
@@ -9,21 +9,20 @@ var config = {
         jquery: {
             exports: '$'
         },
-        'Smartwave_Megamenu/js/sw_megamenu':
-            {
-                deps: ['jquery']
-            }, 
-        'owl.carousel/owl.carousel.min':
-            {
-                deps: ['jquery']
-            },
-        'js/jquery.stellar.min': 
-            {
+        'Smartwave_Megamenu/js/sw_megamenu': {
             deps: ['jquery']
-            },
-        'js/jquery.parallax.min':
-            {
+        },
+        'owl.carousel/owl.carousel.min': {
             deps: ['jquery']
-            }
+        },
+        'js/jquery.stellar.min': {
+            deps: ['jquery']
+        },
+        'js/jquery.parallax.min': {
+            deps: ['jquery']
+        },
+        'Magento_Catalog/js/jquery.zoom.min': {
+            deps: ['jquery']
+        }
     }
 };
