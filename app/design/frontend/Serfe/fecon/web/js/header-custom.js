295ecf41f61c171df62d9af45c360c27486b48a0
Bruno Binci <bruno@serfe.com>
2018-06-14 17:31:52 -0300

    54766: [T][I1] - Menu| Working on menu styles

--- app/design/frontend/Serfe/fecon/web/js/header-custom.js
+++ app/design/frontend/Serfe/fecon/web/js/header-custom.js
@@ -9,7 +9,7 @@ function($) {
       $('.magnifying-glass').animate({'width': '200px'});
     })
     $('#search').on('blur', function() {
-      $('.magnifying-glass').animate({'width': '37px'});
+      $('.magnifying-glass').animate({'width': '49px'});
     })
   });
   return;
