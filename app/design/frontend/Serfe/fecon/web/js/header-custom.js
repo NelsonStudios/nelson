e056c36c2f9035efb148a53fc3d235ccf1cf2aa6
Bruno Binci <bruno@serfe.com>
2018-06-19 12:42:23 -0300

    54766: [T][I1] - Menu| Working on menu styles - responsive - search functionality on mobile

--- app/design/frontend/Serfe/fecon/web/js/header-custom.js
+++ app/design/frontend/Serfe/fecon/web/js/header-custom.js
@@ -5,18 +5,20 @@ function($) {
   "use strict";
 
   $(document).ready(function($){
-    $('#search').on('focus', function() {
+    $('.magnifying-glass').on('click', function() {
+      $('.toplinks').fadeOut('fast');
       setTimeout(function(){
         $('.magnifying-glass').animate({'width': '200px'});
+        $('#search').focus();
       }, 400);
-      $('.toplinks').fadeOut('slow');
-    })
+    });
+
     $('#search').on('blur', function() {
       setTimeout(function(){
         $('.toplinks').fadeIn('slow');
       }, 300);
       $('.magnifying-glass').animate({'width': '49px'}, 400);
-    })
+    });
   });
   return;
 });
