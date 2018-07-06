20c97ea2d039c66b7020470a50cb3e0dd2700c7b
xuan <xuan@serfe.com>
2018-07-06 17:46:44 -0300

    0053767 - add helper to create a preorder

--- app/code/Serfe/Shipping/Helper/PreorderHelper.php
+++ app/code/Serfe/Shipping/Helper/PreorderHelper.php
@@ -1,18 +1,67 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 namespace Serfe\Shipping\Helper;
 
 /**
- * Description of PreorderHelper
+ * Helper to create PreorderHelper from quote's data
  *
  * @author Xuan Villagran <xuan@serfe.com>
  */
-class PreorderHelper
+class PreorderHelper extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    //put your code here
+
+    protected $customerSession;
+    
+    protected $preorderFactory;
+
+    protected $preorderRepository;
+
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Customer\Model\Session $customerSession,
+        \Serfe\Shipping\Model\PreorderFactory $preorderFactory,
+        \Serfe\Shipping\Api\PreorderRepositoryInterface $preorderRepository
+    ) {
+        $this->customerSession = $customerSession;
+        $this->preorderFactory = $preorderFactory;
+        $this->preorderRepository = $preorderRepository;
+
+        parent::__construct($context);
+    }
+
+    public function createPreorder(\Magento\Quote\Model\Quote $quote)
+    {
+        $preorderData = $this->getPreorderData($quote);
+        $preorder = $this->preorderFactory->create();
+        $preorder->addData($preorderData);
+        
+        try {
+            $created = $this->preorderRepository->save($preorder);
+        } catch (\Exception $exc) {
+//            echo $exc->getTraceAsString();
+            $this->_logger->error($exc->getMessage());
+            $created = false;
+        }
+        
+        return $created;
+    }
+
+    protected function getPreorderData(\Magento\Quote\Model\Quote $quote)
+    {
+        $customerId = $this->customerSession->getCustomer()->getId();
+        $shippingMethod = $quote->getShippingAddress()->getShippingMethod();
+        $quoteId = $quote->getId();
+        $addressId = $quote->getShippingAddress()->getId();
+        
+        
+        $preorderData = [
+            \Serfe\Shipping\Api\Data\PreorderInterface::IS_AVAILABLE => false,
+            \Serfe\Shipping\Api\Data\PreorderInterface::CUSTOMER_ID => $customerId,
+            \Serfe\Shipping\Api\Data\PreorderInterface::SHIPPING_METHOD => $shippingMethod,
+            \Serfe\Shipping\Api\Data\PreorderInterface::QUOTE_ID => $quoteId,
+            \Serfe\Shipping\Api\Data\PreorderInterface::ADDRESS_ID => $addressId
+        ];
+        
+        return $preorderData;
+    }
 }
