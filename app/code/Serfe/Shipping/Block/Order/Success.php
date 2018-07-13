2a52968c386d9068d7029948c4feb8bbb872e294
xuan <xuan@serfe.com>
2018-07-13 18:00:49 -0300

    0052834 - add success page when request quote is submitted correctly

---
+++ app/code/Serfe/Shipping/Block/Order/Success.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Serfe\Shipping\Block\Order;
+
+/**
+ * Block for success view
+ *
+ * @author Xuan Villagran <xuan@serfe.com>
+ */
+class Success extends \Magento\Framework\View\Element\Template
+{
+
+}
