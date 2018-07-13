fa473de99f8e8e28f837b3a09ef128701597acab
xuan <xuan@serfe.com>
2018-07-13 17:58:52 -0300

    0052834 - modify index controller to autologin the user and redirect to checkout when token is correct

--- app/code/Serfe/Shipping/Controller/Order/Index.php
+++ app/code/Serfe/Shipping/Controller/Order/Index.php
@@ -18,51 +18,24 @@ class Index extends \Magento\Framework\App\Action\Action
 
     /**
      *
-     * @var \Magento\Checkout\Model\Session 
-     */
-    protected $_checkoutSession;
-
-    /**
-     *
      * @var \Serfe\Shipping\Helper\CustomerHelper 
      */
     protected $customerHelper;
-
-    /**
-     *
-     * @var \Serfe\Shipping\Helper\PreorderHelper 
-     */
-    protected $preorderHelper;
-
-    /**
-     *
-     * @var \Magento\Framework\Json\Helper\Data 
-     */
-    protected $jsonHelper;
 
     /**
      * Constructor
      *
      * @param \Magento\Framework\App\Action\Context $context
      * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
-     * @param \Magento\Checkout\Model\Session $_checkoutSession
      * @param \Serfe\Shipping\Helper\CustomerHelper $customerHelper
-     * @param \Serfe\Shipping\Helper\PreorderHelper $preorderHelper
-     * @param \Magento\Framework\Json\Helper\Data $jsonHelper
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\View\Result\PageFactory $resultPageFactory,
-        \Magento\Checkout\Model\Session $_checkoutSession,
-        \Serfe\Shipping\Helper\CustomerHelper $customerHelper,
-        \Serfe\Shipping\Helper\PreorderHelper $preorderHelper,
-        \Magento\Framework\Json\Helper\Data $jsonHelper
+        \Serfe\Shipping\Helper\CustomerHelper $customerHelper
     ) {
         $this->resultPageFactory = $resultPageFactory;
-        $this->_checkoutSession = $_checkoutSession;
         $this->customerHelper = $customerHelper;
-        $this->preorderHelper = $preorderHelper;
-        $this->jsonHelper = $jsonHelper;
 
         parent::__construct($context);
     }
@@ -74,41 +47,14 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-        $success = false;
+        $token = (string) $this->getRequest()->getParam('token');
+        $customerId = (int) $this->getRequest()->getParam('id');
+        
+        $login = $this->customerHelper->loginUserByToken($customerId, $token);
+        $resultRedirect = $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_REDIRECT);
+        $redirectPath = $login ? 'checkout' : '/';
+        $resultRedirect->setPath($redirectPath);
 
-        if ($this->_checkoutSession->isSessionExists() && $this->_checkoutSession->isSessionExists()) {
-            $quote = $this->_checkoutSession->getQuote();
-            $email = $this->getRequest()->getParam('email');
-            $login = $this->customerHelper->autoLoginUser($quote, $email);
-            $preorder = $this->preorderHelper->createPreorder($quote);
-
-            if ($preorder && $login) {
-                $success = true;
-            }
-            $response = [
-                'success' => $success
-            ];
-        }
-
-        try {
-            return $this->jsonResponse($response);
-        } catch (\Magento\Framework\Exception\LocalizedException $e) {
-            return $this->jsonResponse($e->getMessage());
-        } catch (\Exception $e) {
-            $this->logger->critical($e);
-            return $this->jsonResponse($e->getMessage());
-        }
-    }
-
-    /**
-     * Create json response
-     *
-     * @return \Magento\Framework\Controller\ResultInterface
-     */
-    public function jsonResponse($response = '')
-    {
-        return $this->getResponse()->representJson(
-                $this->jsonHelper->jsonEncode($response)
-        );
+        return $resultRedirect;
     }
 }
