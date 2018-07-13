a33936c8d15bb847fdd636c86ac75cd48a280770
xuan <xuan@serfe.com>
2018-07-13 18:04:30 -0300

    0052834 - change preorder to available and send email when shipping price is set

--- app/code/Serfe/Shipping/Model/ResourceModel/Preorder.php
+++ app/code/Serfe/Shipping/Model/ResourceModel/Preorder.php
@@ -5,6 +5,16 @@ namespace Serfe\Shipping\Model\ResourceModel;
 
 class Preorder extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 {
+    protected $customerHelper;
+
+    public function __construct(
+        \Magento\Framework\Model\ResourceModel\Db\Context $context,
+        \Serfe\Shipping\Helper\CustomerHelper $customerHelper,
+        $connectionName = null
+    ) {
+        $this->customerHelper = $customerHelper;
+        parent::__construct($context, $connectionName);
+    }
 
     /**
      * Define resource model
@@ -14,5 +24,18 @@ class Preorder extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
     protected function _construct()
     {
         $this->_init('serfe_shipping_preorder', 'preorder_id');
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected function _beforeSave(\Magento\Framework\Model\AbstractModel $object)
+    {
+        if ($object->dataHasChangedFor('shipping_price')) {
+            $object->setData(\Serfe\Shipping\Api\Data\PreorderInterface::IS_AVAILABLE, 1);
+            $this->customerHelper->addOrderTokenToCustomer($object->getCustomerId());
+        }
+
+        parent::_beforeSave($object);
     }
 }
