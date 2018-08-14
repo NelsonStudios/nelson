d7aee23299fe061e5d8d885b84e23e83a3f3cdf4
Bruno Binci <bruno@serfe.com>
2018-08-14 14:44:58 -0300

    56028: [T][I5][Extra] - Blog addition - Adding windows rezise event in order to detect window change and change style for Windows O.S.

--- app/design/frontend/Fecon/theme/web/js/header-custom.js
+++ app/design/frontend/Fecon/theme/web/js/header-custom.js
@@ -13,31 +13,40 @@ function($) {
         $('#search').focus();
       }, 400);
     });
-
-    $('#search').on('blur', function() {
-      setTimeout(function(){
-        $('.toplinks').fadeIn('slow');
-        $('#google_translate_element').fadeIn('slow');
-      }, 300);
-      $('.magnifying-glass').animate({'width': '49px'}, 400);
+    $(window).on('resize', function() {
+      $('#search').on('blur', function() {
+        setTimeout(function(){
+          $('.toplinks').fadeIn('slow');
+          $('#google_translate_element').fadeIn('slow');
+        }, 300);
+        $('.magnifying-glass').animate({'width': '49px'}, 400);
+      });
+      /* Hack to detect windows 10 & Chrome to fix style issue */
+      if (navigator.userAgent.indexOf("Chrome") !== -1 && 
+      navigator.userAgent.indexOf("Windows") !== -1) {
+        if($('body').width() > 767) {
+          $('.shopping-cart').css({
+            'top': '-48px'
+          });
+        } else {
+          $('.shopping-cart').css({
+            'top': '-24px'
+          });
+        }
+      }
+      /* Hack to detect Edge or Firefox under windows 10 to fix style issue */
+      if ((navigator.userAgent.indexOf("Edge") !== -1 || navigator.userAgent.indexOf("Firefox")) && navigator.userAgent.indexOf("Windows") !== -1) {
+        if($('body').width() > 767) {
+          $('.shopping-cart').css({
+            'top': '-49px'
+          });
+        } else {
+          $('.shopping-cart').css({
+            'top': '-25px'
+          });
+        }
+      }
     });
-    /* Hack to detect windows 10 & Chrome to fix style issue */
-    if (navigator.userAgent.indexOf("Chrome") !== -1 && 
-    navigator.userAgent.indexOf("Windows") !== -1) {
-      if($('body').width() > 767) {
-        $('.shopping-cart').css({
-          'top': '-48px'
-        });
-      }
-    }
-    /* Hack to detect Edge or Firefox under windows 10 to fix style issue */
-    if ((navigator.userAgent.indexOf("Edge") !== -1 || navigator.userAgent.indexOf("Firefox")) && navigator.userAgent.indexOf("Windows") !== -1) {
-      if($('body').width() > 767) {
-        $('.shopping-cart').css({
-          'top': '-49px'
-        });
-      }
-    }
   });
   return;
 });
