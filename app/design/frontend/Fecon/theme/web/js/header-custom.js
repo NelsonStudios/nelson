599cded139da22b6b919646728675cb043de5885
Bruno Binci <bruno@serfe.com>
2018-08-15 08:09:14 -0300

    56028: [T][I5][Extra] - Blog addition - fixing chrome issues

--- app/design/frontend/Fecon/theme/web/js/header-custom.js
+++ app/design/frontend/Fecon/theme/web/js/header-custom.js
@@ -6,6 +6,18 @@ function($) {
 
   $(document).ready(function($) {
     function applyFixesOnWindowsBrowsers() {
+      /* Hack to detect Edge or Firefox under windows 10 to fix style issue */
+      if ((navigator.userAgent.indexOf("Edge") !== -1 || navigator.userAgent.indexOf("Firefox")) && navigator.userAgent.indexOf("Windows") !== -1) {
+        if($('body').width() > 767) {
+          $('.shopping-cart').css({
+            'top': '-49px'
+          });
+        } else {
+          $('.shopping-cart').css({
+            'top': '-25px'
+          });
+        }
+      }
       /* Hack to detect windows 10 & Chrome to fix style issue */
       if (navigator.userAgent.indexOf("Chrome") !== -1 && 
       navigator.userAgent.indexOf("Windows") !== -1) {
@@ -16,18 +28,6 @@ function($) {
         } else {
           $('.shopping-cart').css({
             'top': '-24px'
-          });
-        }
-      }
-      /* Hack to detect Edge or Firefox under windows 10 to fix style issue */
-      if ((navigator.userAgent.indexOf("Edge") !== -1 || navigator.userAgent.indexOf("Firefox")) && navigator.userAgent.indexOf("Windows") !== -1) {
-        if($('body').width() > 767) {
-          $('.shopping-cart').css({
-            'top': '-49px'
-          });
-        } else {
-          $('.shopping-cart').css({
-            'top': '-25px'
           });
         }
       }
