1e5f522d94bd64fa5dd80c22d09fa0855127174e
Bruno Binci <bruno@serfe.com>
2018-08-17 09:22:58 -0300

    56028: [T][I5][Extra] - Blog addition - fixing chrome issues

--- app/design/frontend/Fecon/theme/web/js/header-custom.js
+++ app/design/frontend/Fecon/theme/web/js/header-custom.js
@@ -7,7 +7,7 @@ function($) {
   $(document).ready(function($) {
     function applyFixesOnWindowsBrowsers() {
       /* Hack to detect Edge or Firefox under windows 10 to fix style issue */
-      if ((navigator.userAgent.indexOf("Edge") !== -1 || navigator.userAgent.indexOf("Firefox")) && navigator.userAgent.indexOf("Windows") !== -1) {
+      if ((navigator.userAgent.indexOf("Edge") !== -1 || navigator.userAgent.indexOf("Firefox") !== -1) && navigator.userAgent.indexOf("Windows") !== -1) {
         if($('body').width() > 767) {
           $('.shopping-cart').css({
             'top': '-49px'
@@ -19,8 +19,9 @@ function($) {
         }
       }
       /* Hack to detect windows 10 & Chrome to fix style issue */
-      if (navigator.userAgent.indexOf("Chrome") !== -1 && 
-      navigator.userAgent.indexOf("Windows") !== -1) {
+      if (navigator.userAgent.indexOf("Chrome") !== -1 &&
+          navigator.userAgent.indexOf("Edge") === -1 &&
+          navigator.userAgent.indexOf("Windows") !== -1) {
         if($('body').width() > 767) {
           $('.shopping-cart').css({
             'top': '-48px'
