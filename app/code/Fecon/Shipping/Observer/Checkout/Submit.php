9a12c6325bee53cd74560c2a46ed0f5d86bb4438
xuan <xuan@serfe.com>
2018-09-11 16:53:59 -0300

    0056055 - create observer to subscribe to the submit order successfully event and change preorder status

---
+++ app/code/Fecon/Shipping/Observer/Checkout/Submit.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Fecon\Shipping\Observer\Checkout;
+
+/**
+ * Observer that listen to the "submit order successfully" event
+ */
+class Submit implements \Magento\Framework\Event\ObserverInterface
+{
+
+    /**
+     * Preorder Helper
+     *
+     * @var \Fecon\Shipping\Helper\PreorderHelper 
+     */
+    protected $preorderHelper;
+
+    /**
+     * Constructor
+     *
+     * @param \Fecon\Shipping\Helper\PreorderHelper $preorderHelper
+     * @return void
+     */
+    public function __construct(\Fecon\Shipping\Helper\PreorderHelper $preorderHelper)
+    {
+        $this->preorderHelper = $preorderHelper;
+    }
+
+    /**
+     * Execute observer
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(
+        \Magento\Framework\Event\Observer $observer
+    ) {
+        $order = $observer->getOrder();
+        $shippingMethod = $order->getShippingMethod();
+        if (strpos($shippingMethod, 'manualshipping') !== false) {
+            $quote = $observer->getQuote();
+            $this->preorderHelper->completePreorder($quote);
+        }
+    }
+}
