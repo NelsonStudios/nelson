093f3f6d430c5e2c4dba0f4dde286276fcb21c38
xuan <xuan@serfe.com>
2018-11-01 17:14:36 -0300

    0057898 - fix cache tags

--- app/code/Fecon/SytelineIntegration/Model/Cache/Type.php
+++ app/code/Fecon/SytelineIntegration/Model/Cache/Type.php
@@ -11,12 +11,12 @@ class Type extends \Magento\Framework\Cache\Frontend\Decorator\TagScope
     /**
      * Cache type code unique among all cache types
      */
-    const TYPE_IDENTIFIER = 'syteline_cache_identifier';
+    const TYPE_IDENTIFIER = 'syteline_cache';
 
     /**
      * Cache tag used to distinguish the cache type from all other cache
      */
-    const CACHE_TAG = 'SYTELINE_CACHE_TAG';
+    const CACHE_TAG = 'SYTELINE_CACHE';
 
     /**
      * @param \Magento\Framework\App\Cache\Type\FrontendPool $cacheFrontendPool
