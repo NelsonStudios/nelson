81ff592208f4dec5d337e02591cd034f07911a0d
xuan <xuan@serfe.com>
2018-12-18 13:19:19 -0300

    0056483 - add logic to create dropdown attributes

--- app/code/Fecon/OrsProducts/Model/AbstractAttributes.php
+++ app/code/Fecon/OrsProducts/Model/AbstractAttributes.php
@@ -3,11 +3,16 @@
 namespace Fecon\OrsProducts\Model;
 
 /**
- * Description of AbstractAttributes
+ * Abstract class with the logic to handle attributes creation
  */
 abstract class AbstractAttributes
 {
 
+    /**
+     * Get Default options for text attributes
+     *
+     * @return array
+     */
     protected function getDefaultTextValues()
     {
         return [
@@ -32,22 +37,78 @@ abstract class AbstractAttributes
         ];
     }
 
+    /**
+     * Get text attributes with all options defined
+     *
+     * @param array $userDefinedAttributes
+     * @return array
+     */
     protected function getTextAttributes($userDefinedAttributes)
     {
-//        $attributes = [
-//            'unspsc' => [
-//                'label' => 'UNSPSC',
-//                'group' => 'General',
-//            ]
-//        ];
-        $attributes = [];
         $defaultTextValues = $this->getDefaultTextValues();
 
+        return $this->mergeOptions($defaultTextValues, $userDefinedAttributes);
+    }
+
+    /**
+     * Merge default and user defined options for attributes
+     *
+     * @param array $defaultValues
+     * @param array $userDefinedAttributes
+     * @return array
+     */
+    protected function mergeOptions($defaultValues, $userDefinedAttributes)
+    {
+        $attributes = [];
+
         foreach ($userDefinedAttributes as $attributeCode => $values) {
-            $attributeValues = array_merge($defaultTextValues, $values);
+            $attributeValues = array_merge($defaultValues, $values);
             $attributes[$attributeCode] = $attributeValues;
         }
 
         return $attributes;
+    }
+
+    /**
+     * Get Dropdown attributes with all options defined
+     *
+     * @param array $userDefinedAttributes
+     * @return array
+     */
+    protected function getDropdownAttributes($userDefinedAttributes)
+    {
+        $defaultDropdownValues = $this->getDefaultDropdownValues();
+
+        return $this->mergeOptions($defaultDropdownValues, $userDefinedAttributes);
+    }
+
+    /**
+     * Get Default options for dropdown attributes
+     *
+     * @return array
+     */
+    protected function getDefaultDropdownValues()
+    {
+        return [
+            'type' => 'int',
+            'backend' => '',
+            'frontend' => '',
+            'input' => 'select',
+            'class' => '',
+            'source' => '',
+            'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+            'visible' => true,
+            'required' => false,
+            'user_defined' => true,
+            'default' => null,
+            'searchable' => false,
+            'filterable' => false,
+            'comparable' => false,
+            'visible_on_front' => false,
+            'used_in_product_listing' => false,
+            'unique' => false,
+            'apply_to' => '',
+            'system' => 1,
+        ];
     }
 }
