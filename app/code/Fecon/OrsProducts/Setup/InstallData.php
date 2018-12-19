c83f15dd27848f24660dc79d278f2bf965a6ae5b
xuan <xuan@serfe.com>
2018-12-19 17:38:58 -0300

    0056483 - rename attributes namespace

--- app/code/Fecon/OrsProducts/Setup/InstallData.php
+++ app/code/Fecon/OrsProducts/Setup/InstallData.php
@@ -11,199 +11,48 @@ use Magento\Eav\Setup\EavSetupFactory;
 class InstallData implements InstallDataInterface
 {
 
-    private $eavSetupFactory;
+    /**
+     * @var EavSetupFactory 
+     */
+    protected $eavSetupFactory;
+
+    /**
+     * @var \Fecon\OrsProducts\Model\Ors\AttributeSet 
+     */
+    protected $orsAttributeSet;
+
+    /**
+     * @var \Fecon\OrsProducts\Model\Ors\Attributes 
+     */
+    protected $orsAttributes;
 
     /**
      * Constructor
      *
-     * @param \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory
+     * @param EavSetupFactory $eavSetupFactory
+     * @param \Fecon\OrsProducts\Model\Ors\AttributeSet $orsAttributeSet
+     * @param \Fecon\OrsProducts\Model\Ors\Attributes $orsAttributes
      */
-    public function __construct(EavSetupFactory $eavSetupFactory)
-    {
+    public function __construct(
+        EavSetupFactory $eavSetupFactory,
+        \Fecon\OrsProducts\Model\Ors\AttributeSet $orsAttributeSet,
+        \Fecon\OrsProducts\Model\Ors\Attributes $orsAttributes
+    ) {
         $this->eavSetupFactory = $eavSetupFactory;
+        $this->orsAttributeSet = $orsAttributeSet;
+        $this->orsAttributes = $orsAttributes;
     }
 
     /**
      * {@inheritdoc}
      */
     public function install(
-    ModuleDataSetupInterface $setup, ModuleContextInterface $context
-    )
-    {
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        /* @var $eavSetup EavSetup */
         $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
-
-        $eavSetup->addAttribute(
-        \Magento\Catalog\Model\Product::ENTITY,
-        'unspsc',
-        [
-        'type' => 'varchar',
-        'backend' => '',
-        'frontend' => '',
-        'label' => 'UNSPSC',
-        'input' => 'text',
-        'class' => '',
-        'source' => '',
-        'global' => 1,
-        'visible' => true,
-        'required' => false,
-        'user_defined' => true,
-        'default' => null,
-        'searchable' => false,
-        'filterable' => false,
-        'comparable' => false,
-        'visible_on_front' => false,
-        'used_in_product_listing' => false,
-        'unique' => false,
-        'apply_to' => '',
-        'system' => 1,
-        'group' => 'General',
-        'option' => array('values' => array(""))
-        ]
-        );
-
-        $eavSetup->addAttribute(
-        \Magento\Catalog\Model\Product::ENTITY,
-        'upc',
-        [
-        'type' => 'varchar',
-        'backend' => '',
-        'frontend' => '',
-        'label' => 'UPC',
-        'input' => 'text',
-        'class' => '',
-        'source' =>,
-        'global' => 1,
-        'visible' => true,
-        'required' => false,
-        'user_defined' => true,
-        'default' => null,
-        'searchable' => false,
-        'filterable' => false,
-        'comparable' => false,
-        'visible_on_front' => false,
-        'used_in_product_listing' => false,
-        'unique' => false,
-        'apply_to' => '',
-        'system' => 1,
-        'group' => 'General',
-        'option' => array('values' => array(""))
-        ]
-        );
-
-        $eavSetup->addAttribute(
-        \Magento\Catalog\Model\Product::ENTITY,
-        'mfg_part_number',
-        [
-        'type' => 'int',
-        'backend' => '',
-        'frontend' => '',
-        'label' => 'MfgPartNumber',
-        'input' => 'select',
-        'class' => '',
-        'source' =>,
-        'global' => 1,
-        'visible' => true,
-        'required' => false,
-        'user_defined' => true,
-        'default' => null,
-        'searchable' => false,
-        'filterable' => false,
-        'comparable' => false,
-        'visible_on_front' => false,
-        'used_in_product_listing' => false,
-        'unique' => false,
-        'apply_to' => '',
-        'system' => 1,
-        'group' => 'General',
-        'option' => array('values' => array(""))
-        ]
-        );
-
-        $eavSetup->addAttribute(
-        \Magento\Catalog\Model\Product::ENTITY,
-        'web_uom',
-        [
-        'type' => 'int',
-        'backend' => '',
-        'frontend' => '',
-        'label' => 'WebUOM',
-        'input' => 'select',
-        'class' => '',
-        'source' =>,
-        'global' => 1,
-        'visible' => true,
-        'required' => false,
-        'user_defined' => true,
-        'default' => null,
-        'searchable' => false,
-        'filterable' => false,
-        'comparable' => false,
-        'visible_on_front' => false,
-        'used_in_product_listing' => false,
-        'unique' => false,
-        'apply_to' => '',
-        'system' => 1,
-        'group' => 'General',
-        'option' => array('values' => array(""))
-        ]
-        );
-
-        $eavSetup->addAttribute(
-        \Magento\Catalog\Model\Product::ENTITY,
-        'family',
-        [
-        'type' => 'int',
-        'backend' => '',
-        'frontend' => '',
-        'label' => 'Family',
-        'input' => 'select',
-        'class' => '',
-        'source' =>,
-        'global' => 1,
-        'visible' => true,
-        'required' => false,
-        'user_defined' => true,
-        'default' => null,
-        'searchable' => false,
-        'filterable' => false,
-        'comparable' => false,
-        'visible_on_front' => false,
-        'used_in_product_listing' => false,
-        'unique' => false,
-        'apply_to' => '',
-        'system' => 1,
-        'group' => 'General',
-        'option' => array('values' => array(""))
-        ]
-        );
-
-        $eavSetup->addAttribute(
-        \Magento\Catalog\Model\Product::ENTITY,
-        'manufacturer_uRL',
-        [
-        'type' => 'int',
-        'backend' => '',
-        'frontend' => '',
-        'label' => 'Manufacturer URL',
-        'input' => 'select',
-        'class' => '',
-        'source' =>,
-        'global' => 1,
-        'visible' => true,
-        'required' => false,
-        'user_defined' => true,
-        'default' => null,
-        'searchable' => false,
-        'filterable' => false,
-        'comparable' => false,
-        'visible_on_front' => false,
-        'used_in_product_listing' => false,
-        'unique' => false,
-        'apply_to' => '',
-        'system' => 1,
-        'group' => 'General',
-        'option' => array('values' => array(""))
-        ]
-        );
+        $this->orsAttributes->createAttributes($eavSetup);
+        $this->orsAttributeSet->createStructure($eavSetup);
     }
 }
