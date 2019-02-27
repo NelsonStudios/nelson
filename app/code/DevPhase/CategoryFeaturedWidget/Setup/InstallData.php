ab9395898f47b0fb276a680ae2111441bd6bf9ad
Matt Healy <matt@devphase.io>
2019-02-27 21:11:10 +0000

    merged featured category attribute module with frontend output widget

---
+++ app/code/DevPhase/CategoryFeaturedWidget/Setup/InstallData.php
@@ -0,0 +1,49 @@
+<?php
+/* file: app/code/DevPhase/CategoryFeaturedWidget/Setup/InstallData.php */
+
+namespace DevPhase\CategoryFeaturedWidget\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     *
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(\Magento\Catalog\Model\Category::ENTITY, 'featured_category', [
+            'type'     => 'int',
+            'label'    => 'Featured Category',
+            'input'    => 'boolean',
+            'source'   => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+            'visible'  => true,
+            'default'  => '0',
+            'required' => false,
+            'global'   => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+            'group'    => 'Display Settings',
+        ]);
+    }
+}
---
+++ app/code/DevPhase/CategoryFeaturedWidget/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'DevPhase_CategoryFeaturedWidget',
+    __DIR__
+);
