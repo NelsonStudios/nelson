6a614489f9d6f6af5eae8ef53a07296c7185477b
xuan <xuan@serfe.com>
2019-03-21 15:56:36 -0300

    0061567 - add link with Organization entity values and the customer attribute

---
+++ app/code/Fecon/Sso/Model/Organization/Options.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Fecon\Sso\Model\Organization;
+
+use Fecon\Sso\Api\OrganizationRepositoryInterface;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+
+/**
+ * Get Organization options
+ */
+class Options implements \Magento\Framework\Data\OptionSourceInterface
+{
+
+    /**
+     * @var null|array
+     */
+    protected $options;
+
+    /**
+     * @var OrganizationRepositoryInterface
+     */
+    protected $organizationRepository;
+
+    /**
+     * @var FilterBuilder
+     */
+    protected $filterBuilder;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    protected $searchCriteriaBuilder;
+
+    /**
+     * Constructor
+     *
+     * @param OrganizationRepositoryInterface $organizationRepository
+     * @param FilterBuilder $filterBuilder
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     */
+    public function __construct(
+        OrganizationRepositoryInterface $organizationRepository,
+        FilterBuilder $filterBuilder,
+        SearchCriteriaBuilder $searchCriteriaBuilder
+    ) {
+        $this->organizationRepository = $organizationRepository;
+        $this->filterBuilder = $filterBuilder;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function toOptionArray()
+    {
+        $this->options = [
+            [
+                'value' => '',
+                'label' => '-- Select Organization --'
+            ]
+        ];
+        try {
+            $filter =  $this->filterBuilder->setField('organization_id')
+                ->setValue('%')
+                ->setConditionType('like')
+                ->create();
+            $searchCriteria = $this->searchCriteriaBuilder->addFilters([$filter])->create();
+            $organizationResult = $this->organizationRepository->getList($searchCriteria);
+            $organizationList = $organizationResult->getItems();
+            foreach ($organizationList as $organization) {
+                $this->options[] = [
+                    'value' => $organization->getOrganizationId(),
+                    'label' => $organization->getName()
+                ];
+            }
+        } catch (\Exception $ex) { }
+
+        return $this->options;
+    }
+}
