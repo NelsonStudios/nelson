e26401d65afc81b4099403971e295aa07d7253a4
xuan <xuan@serfe.com>
2019-03-27 13:00:42 -0300

    0061567 - fix button lable

--- app/code/Fecon/Sso/Block/Adminhtml/UserGroup/Edit/SaveButton.php
+++ app/code/Fecon/Sso/Block/Adminhtml/UserGroup/Edit/SaveButton.php
@@ -14,7 +14,7 @@ class SaveButton extends GenericButton implements ButtonProviderInterface
     public function getButtonData()
     {
         return [
-            'label' => __('Save Usergroup'),
+            'label' => __('Save User Group'),
             'class' => 'save primary',
             'data_attribute' => [
                 'mage-init' => ['button' => ['event' => 'save']],
