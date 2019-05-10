1b919c2442339abfe6c8d021f5aba2083cbbe7b7
xuan <xuan@serfe.com>
2019-05-10 13:11:40 -0300

    0062195 - override cart blocks

---
+++ app/code/Fecon/SytelineIntegration/Block/Cart/Grid.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Fecon\SytelineIntegration\Block\Cart;
+
+/**
+ * Description of Grid
+ */
+class Grid extends \Magento\Checkout\Block\Cart\Grid
+{
+
+}
---
+++ app/code/Fecon/SytelineIntegration/Block/Cart/Item/Renderer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Fecon\SytelineIntegration\Block\Cart\Item;
+
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Framework\View\Element\Message\InterpretationStrategyInterface;
+
+class Renderer extends \Magento\Checkout\Block\Cart\Item\Renderer
+{
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Catalog\Helper\Product\Configuration $productConfig,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Catalog\Block\Product\ImageBuilder $imageBuilder,
+        \Magento\Framework\Url\Helper\Data $urlHelper,
+        \Magento\Framework\Message\ManagerInterface $messageManager,
+        PriceCurrencyInterface $priceCurrency,
+        \Magento\Framework\Module\Manager $moduleManager,
+        InterpretationStrategyInterface $messageInterpretationStrategy,
+        array $data = array()
+    ) {
+        parent::__construct($context, $productConfig, $checkoutSession, $imageBuilder, $urlHelper, $messageManager, $priceCurrency, $moduleManager, $messageInterpretationStrategy, $data);
+    }
+}
