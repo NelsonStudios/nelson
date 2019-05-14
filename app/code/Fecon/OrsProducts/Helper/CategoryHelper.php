3d555eecda9b0a44e7a00e567b3b0f1c1657c570
xuan <xuan@serfe.com>
2019-05-14 10:53:12 -0300

    0061781 - create category helper

---
+++ app/code/Fecon/OrsProducts/Helper/CategoryHelper.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Fecon\OrsProducts\Helper;
+
+/**
+ * CategoryHelper
+ */
+class CategoryHelper
+{
+
+    protected $objectManager;
+
+    protected $collecionFactory;
+
+    public function __construct(
+        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $collecionFactory
+    ) {
+        $this->collectionFactory = $collecionFactory;
+        $this->objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+    }
+
+    public function createCategory($path)
+    {
+        $categoryName = array_pop($path);
+        $parentCategory = $this->getCategory($path);
+        $category = $this->objectManager->create(\Magento\Catalog\Model\Category::class);
+        $data = [
+            'name' => $categoryName,
+            "is_active" => true,
+            "include_in_menu" => false,
+        ];
+        $category->addData($data);
+        $category->setPath($parentCategory->getPath());
+        $category->setParentId($parentCategory->getId());
+        $category->setAttributeSetId($category->getDefaultAttributeSetId());
+        $category->save();
+
+        return $category;
+    }
+
+    public function getCategory($path)
+    {
+        $category = false;
+        $categoryName = array_pop($path);
+        $collection = $this->collecionFactory
+            ->create()
+            ->addAttributeToFilter('name', $categoryName)
+            ->setPageSize(1);
+
+        if ($collection->getSize()) {
+            $categoryId = $collection->getFirstItem()->getId();
+            $category = $this->objectManager->create(\Magento\Catalog\Model\Category::class);
+            $category->load($categoryId);
+        } else {
+            array_push($path, $categoryName);
+            $category = $this->createCategory($path);
+        }
+
+        return $category;
+    }
+}
