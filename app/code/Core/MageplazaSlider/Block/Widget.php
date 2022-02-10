ca9bc8a4e5dbdf25309972910b71e3b1a28b5829
Hai Internal <Hai+Internal@coredevelop.com>
2022-02-10 16:59:41 +0700

    fix banner slider

--- app/code/Core/MageplazaSlider/Block/Widget.php
+++ app/code/Core/MageplazaSlider/Block/Widget.php
@@ -13,7 +13,7 @@ class Widget extends \Mageplaza\BannerSlider\Block\Widget
     public $_storeManager;
     protected $_filesystem ;
     protected $_imageFactory;
-    
+
     public function __construct(
         Template\Context $context,
         bannerHelper $helperData,
@@ -21,12 +21,12 @@ class Widget extends \Mageplaza\BannerSlider\Block\Widget
         DateTime $dateTime,
         FilterProvider $filterProvider,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Framework\Filesystem $filesystem,         
+        \Magento\Framework\Filesystem $filesystem,
         \Magento\Framework\Image\AdapterFactory $imageFactory,
         array $data = []
     ) {
         $this->_storeManager=$storeManager;
-        $this->_filesystem = $filesystem;               
+        $this->_filesystem = $filesystem;
         $this->_imageFactory = $imageFactory;
         parent::__construct($context,$helperData,$customerRepository,$dateTime,$filterProvider,$data);
     }
@@ -42,7 +42,7 @@ class Widget extends \Mageplaza\BannerSlider\Block\Widget
             return false;
         }
 
-        $sliders = $this->helperData->getCustomSlider();
+        $sliders = $this->helperData->getActiveSliders();
         foreach ($sliders as $slider) {
             if ($slider->getData('location') != 'custom') {
                 continue;
@@ -68,19 +68,19 @@ class Widget extends \Mageplaza\BannerSlider\Block\Widget
         $imageResized = $this->_filesystem->getDirectoryRead(\Magento\Framework\App\Filesystem\DirectoryList::MEDIA)->getAbsolutePath('resized/'.$width.'/').$image;
         if (!file_exists($imageResized)) { // Only resize image if not already exists.
             //create image factory...
-            $imageResize = $this->_imageFactory->create();         
+            $imageResize = $this->_imageFactory->create();
             $imageResize->open($absolutePath);
-            $imageResize->constrainOnly(TRUE);         
-            $imageResize->keepTransparency(TRUE);         
-            $imageResize->keepFrame(FALSE);         
-            $imageResize->keepAspectRatio(TRUE);         
-            $imageResize->resize($width,$height);  
-            //destination folder                
-            $destination = $imageResized ;    
-            //save image      
-            $imageResize->save($destination);         
-        } 
+            $imageResize->constrainOnly(TRUE);
+            $imageResize->keepTransparency(TRUE);
+            $imageResize->keepFrame(FALSE);
+            $imageResize->keepAspectRatio(TRUE);
+            $imageResize->resize($width,$height);
+            //destination folder
+            $destination = $imageResized ;
+            //save image
+            $imageResize->save($destination);
+        }
         $resizedURL = $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA).'resized/'.$width.'/'.$image;
         return $resizedURL;
-  } 
+  }
 }
